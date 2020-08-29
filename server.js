f3bdceaeb71ad34bce29adf6be60088f2f8f8573
Yogesh-Mujawdiya <ykmujawdiya@gmail.com>
2020-08-30 01:41:00 +0530

    1st

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ res.sendFile(path.join(__dirname,'/dist/Find-My-Doctor/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });

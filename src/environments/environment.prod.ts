9007363d8d6c8356846b96c6b9297315947941a6
Yogesh-Mujawdiya <ykmujawdiya@gmail.com>
2020-08-31 12:03:36 +0530

    1st

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // HostUrl: 'https://find-my-doctor.000webhostapp.com'
-  HostUrl: 'http://localhost/FindMyDoctor'
+  HostUrl: 'https://find-my-doctor.000webhostapp.com'
+  // HostUrl: 'http://localhost/FindMyDoctor'
 };
